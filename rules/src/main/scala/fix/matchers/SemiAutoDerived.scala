31a6b2b5bd07aab3da60aa005d0c523a752b6349
Ingar Abrahamsen <ingar@abrahams1.com>
2022-12-16 16:36:55 +0100

    refactor: move matchers out of rules

---
+++ rules/src/main/scala/fix/matchers/SemiAutoDerived.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Arktekk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fix.matchers
+
+import scalafix.v1.SemanticDocument
+
+import scala.meta._
+import scalafix.v1._
+
+case class SemiAutoDerived(
+    deriveType: String,
+    defn: Defn
+)
+
+object SemiAutoDerived {
+
+  def unapply(o: Defn.Object)(implicit doc: SemanticDocument): Option[List[SemiAutoDerived]] =
+    nonEmptyList(o.templ.stats.collect {
+      case g @ Defn.GivenAlias(_, _, _, _, typeApply @ Type.Apply(_, (typeName: Type.Name) :: Nil), body)
+          if matchingType(o, typeName) && isSemiAuto(body) =>
+        SemiAutoDerived(typeApply.symbol.normalized.value.dropRight(1), g)
+      case v @ Defn.Val(mods, _, Some(applied @ Type.Apply(_, (typeName: Type.Name) :: Nil)), body)
+          if matchingType(o, typeName) && mods.exists(_.is[Mod.Implicit]) && isSemiAuto(body) =>
+        SemiAutoDerived(findSymbolFromSignature(v).getOrElse(applied.symbol).normalized.value.dropRight(1), v)
+      case v @ Defn.Def(mods, _, _, _, Some(applied @ Type.Apply(_, (typeName: Type.Name) :: Nil)), body)
+          if matchingType(o, typeName) && mods.exists(_.is[Mod.Implicit]) && isSemiAuto(body) =>
+        SemiAutoDerived(findSymbolFromSignature(v).getOrElse(applied.symbol).normalized.value.dropRight(1), v)
+    })
+
+  private def matchingType(o: Defn.Object, typeName: Type.Name): Boolean =
+    typeName.value == o.name.value
+
+  private def isSemiAuto(t: Term)(implicit doc: SemanticDocument) = {
+    t.symbol.normalized.value.contains(".semiauto.") || t.symbol.normalized.value.contains("derived")
+  }
+
+  private def nonEmptyList[A](l: List[A]): Option[List[A]] =
+    if (l.isEmpty) None else Some(l)
+
+  private def findSymbolFromSignature(defn: Defn)(implicit doc: SemanticDocument) = {
+    val sym = defn.symbol
+    sym.info.map(_.signature).flatMap {
+      case ValueSignature(TypeRef(_, symbol, _)) =>
+        Some(symbol)
+      case _ => None
+    }
+  }
+
+}
